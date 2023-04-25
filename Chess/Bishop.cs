0691d2909fdc819722bb19dc02de7b0699490cf1
ColeDrum18 <125422997+ColeDrum18@users.noreply.github.com>
2023-04-25 13:46:31 -0500

    Add files via upload

--- Chess/Bishop.cs
+++ Chess/Bishop.cs
@@ -13,28 +13,31 @@ namespace Chess
         {
             Char[] aToH = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h' };
             char file = moveTo[0];
-            char lfile = (char)(file - 1);
-            char l2file = (char)(file - 2);
-            char rfile = (char)(file + 1);
-            char r2file = (char)(file + 2);
             int row = int.Parse(moveTo[1].ToString());
-            int uprow = row + 1;
-            int up2row = row + 2;
-            int downrow = row - 1;
-            int down2row = row - 2;
-            
-            
+            string[] from = { (char)(file+1) + (row+1).ToString(), (char)(file + 2) + (row + 2).ToString(), (char)(file + 3) + (row + 3).ToString(), (char)(file + 4) + (row + 4).ToString(), (char)(file + 5) + (row + 5).ToString(), (char)(file + 6) + (row + 6).ToString(), (char)(file + 7) + (row + 7).ToString(),
+                               (char)(file - 1) + (row+1).ToString(), (char)(file - 2) + (row + 2).ToString(), (char)(file - 3) + (row + 3).ToString(), (char)(file - 4) + (row + 4).ToString(), (char)(file - 5) + (row + 5).ToString(), (char)(file - 6) + (row + 6).ToString(), (char)(file - 7) + (row + 7).ToString(),
+                               (char)(file+1) + (row-1).ToString(), (char)(file + 2) + (row - 2).ToString(), (char)(file + 3) + (row - 3).ToString(), (char)(file + 4) + (row - 4).ToString(), (char)(file + 5) + (row - 5).ToString(), (char)(file + 6) + (row - 6).ToString(), (char)(file + 7) + (row - 7).ToString(),
+                               (char)(file-1) + (row-1).ToString(), (char)(file - 2) + (row - 2).ToString(), (char)(file - 3) + (row - 3).ToString(), (char)(file - 4) + (row - 4).ToString(), (char)(file - 5) + (row - 5).ToString(), (char)(file - 6) + (row - 6).ToString(), (char)(file - 7) + (row - 7).ToString(),};
+            //for(int i = 0; i < from.Length; i++)
+            //{
+              //  Console.WriteLine(from[i]);
+                //if (!aToH.Contains(from[i][0]))
+                //{
+                 //   from[i] = "";
+                //}
+                
+            //}
 
-
-            return null;
+            return from;
         }
 
         public static string findControl(Control[] controls, Control moveTo)
         {
-            string knight = "";
+            string bishop = "";
             string tag = (string)moveTo.Tag;
             int x = controls.Length;
             string color;
+            int index = 0;
             if (Game.turn % 2 == 1)
             {
                 color = "white";
@@ -45,25 +48,47 @@ namespace Chess
             }
             for (int i = 0; i < controls.Length; i++)
             {
-                if (controls[i] != null && (string)controls[i].Tag == (color + " knight"))
+                if (controls[i] != null && (string)controls[i].Tag == (color + " bishop"))
                 {
                     if (tag == null)
                     {
-                        knight = controls[i].Name;
+                        bishop = controls[i].Name;
+                        index = i;
                         break;
                     }
                     else if (!tag.Contains(color))
                     {
-                        knight = controls[i].Name;
+                        bishop = controls[i].Name;
+                        index = i;
                         break;
                     }
                     else
                     {
-                        knight = "";
+                        bishop = "";
                     }
                 }
             }
-            return knight;
+            int start = 0;
+            if (index < 7)
+                start = 0;
+            else if (index < 14)
+                start = 7;
+            else if (index < 21)
+                start = 14;
+            else
+                start = 21;
+            for(int i = start; i < index; i++)
+            {
+                if (controls[i] != null)
+                {
+                    if (controls[i].BackgroundImage != null)
+                    {
+                        bishop = "";
+                        break;
+                    }
+                }
+            }
+            return bishop;
         }
     }
 }
