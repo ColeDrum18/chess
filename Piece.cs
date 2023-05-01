96992a21ba3395ca7ff7e7ac6f4d729b975c5d80
ColeDrum18 <125422997+ColeDrum18@users.noreply.github.com>
2023-05-01 15:24:14 -0500

    Add files via upload

---
+++ Piece.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chess
+{
+    abstract class Piece
+    {
+        // team 1 = white, team 2 = black
+        public abstract string[] movement();
+        public abstract string findControl(Control[] controls);
+    }
+}
