ed2675eb462abb51d75059f2e64bf499dbda51ea
ColeDrum18 <125422997+ColeDrum18@users.noreply.github.com>
2023-05-04 10:03:00 -0500

    Add files via upload
    Had to fix EnPassant, now the game is complete

--- Chess/Game.cs
+++ Chess/Game.cs
@@ -11,13 +11,15 @@ namespace Chess
 {
     internal class Game
     {
-        public static int turn { get; set; } = 1;
-        public static bool EP { get; set; } = false;
+        public static int turn { get { return _turn; } set { _turn = value; } }
+        private static int _turn = 1;
+        public static bool EP { get { return _EP; } set { _EP = value; } }
+        private static bool _EP = false;
 
         public static bool LongCastle(Control a1,Control b1, Control c1, Control d1, Control e1)
         {
             string color;
-            if (turn % 2 == 1)
+            if (_turn % 2 == 1)
                 color = "white";
             else
                 color = "black";
@@ -32,7 +34,7 @@ namespace Chess
         public static void ShortCastle(Control h1, Control g1, Control f1, Control e1)
         {
             string color;
-            if (turn % 2 == 1)
+            if (_turn % 2 == 1)
                 color = "white";
             else
                 color = "black";
@@ -47,7 +49,7 @@ namespace Chess
                 f1.BackgroundImage = Resources.whiteRook;
                 f1.Tag = "white rook";
                 EP = false;
-                turn++;
+                _turn++;
             }
             
         }
@@ -56,7 +58,7 @@ namespace Chess
         {
             string color;
             System.Drawing.Bitmap image;
-            if(turn %2 == 1)
+            if(_turn %2 == 1)
             {
                 color = "white";
                 image = Resources.whitePawn;
@@ -72,13 +74,14 @@ namespace Chess
             moveTo.Tag = color + " pawn";
             current.BackgroundImage = null;
             current.Tag = null;
+            _turn++;
 
         }
 
         public static System.Drawing.Bitmap Promote(string moveTo)
         {
             System.Drawing.Bitmap image;
-            if(turn % 2 == 1)
+            if(_turn % 2 == 1)
             {
                 if (moveTo[1] == '8')
                 {
